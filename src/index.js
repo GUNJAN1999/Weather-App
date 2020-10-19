cb7cec19871c6e027420b686ee30e1ae2389386b
GUNJAN1999 <gunjanbbps62@gmail.com>
2020-10-19 23:17:03 +0530

    Initial commit of weather app

---
+++ src/index.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import SeasonDisplay from './SeasonDisplay';
+
+
+class App extends React.Component{
+	constructor(props)
+	{
+		super(props);
+		this.state={lat:null,errorMessage:''};
+		
+	}
+	/*
+	ALTERNATE METHOD TO INITLIZE THE STATE
+	state={lat:null,errorMessage:''};
+	
+	 */
+	componentDidMount()
+	{
+		window.navigator.geolocation.getCurrentPosition((position)=>
+			this.setState({lat:position.coords.latitude}),
+			(err)=>this.setState({errorMessage:err.message})
+		);
+	}
+
+
+	render(){
+		if(this.state.errorMessage && !this.state.lat)
+		{
+			return (
+			<div>Error:{this.state.errorMessage}<hr/></div>
+			);
+		}
+		else if(this.state.lat)
+		{
+			return (
+				<SeasonDisplay lat={this.state.lat}/>
+			);
+		}
+		else{
+			return(
+				<div>LOADING!!-------</div>
+			);
+		}
+
+	}
+}
+ReactDOM.render(<App/>,document.querySelector("#root"));
